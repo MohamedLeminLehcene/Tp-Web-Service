42622b015321a87b769295a09af49c1aca3c26ae
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-04-01 03:22:35 +0000

    Créer un server avec jWS pour déployer notre WebService

--- src/main/java/ma/enset/sdia/ServerJWS.java
+++ src/main/java/ma/enset/sdia/ServerJWS.java
@@ -8,6 +8,7 @@ public class ServerJWS {
 
     public static void main(String[] args)
     {
+
         //2. Déployer le Web service avec un simple Serveur JaxWS
         //address => O.O.O.O : pour que le server permet d'accepter la connéxion depuis n'importe que address IP sur le port 9191
         Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
